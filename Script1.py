9c905ce33f66a437ed24b1fe5723740cee66f3a0
wwlouis00 <wwlouis00@gmail.com>
2022-03-15 09:29:35 +0800

    Update 20220315

---
+++ Script1.py
@@ -0,0 +1,6 @@
+
+def func1():
+    print("Function 1 is active")
+
+if __name__ == '__main__':
+    func1()
---
+++ Script2.py
@@ -0,0 +1,9 @@
+
+import Script1
+
+def func2():
+    print("Function 2 is active")
+
+if __name__ == '__main__':
+    func2()
+    Script1.func1()
